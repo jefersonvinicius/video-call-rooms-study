6b02928af2bc975de6b2f2587957b8294e4a7c1d
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-09-13 22:22:50 -0300

    Add SocketsCollection class

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,8 @@ import { Server, Socket } from 'socket.io';
 import { CONFIG } from './config';
 import { Room } from './entities/room';
 import { User } from './entities/user';
+import { log } from './shared/logging';
 import { VideoStream } from './video-stream';
-
-function log(message: string) {
-  console.log(`[LOG ${new Date().toISOString()}]: ${message}`);
-}
 
 const app = express();
 app.use(cors());
@@ -25,8 +22,36 @@ class UserSocket {
   constructor(readonly user: User, readonly socket: Socket) {}
 }
 
+class UserSocketCollection {
+  private socketIdToUserSocket = new Map<string, UserSocket>();
+  private userIdToUserSocket = new Map<string, UserSocket>();
+
+  add(userSocket: UserSocket) {
+    this.socketIdToUserSocket.set(userSocket.socket.id, userSocket);
+    this.userIdToUserSocket.set(userSocket.user.id, userSocket);
+  }
+
+  getBySocketId(socketId: string) {
+    return this.socketIdToUserSocket.get(socketId);
+  }
+
+  getByUserId(userId: string) {
+    return this.userIdToUserSocket.get(userId);
+  }
+
+  delete(socketId: string) {
+    const user = this.getBySocketId(socketId)?.user;
+    if (user) this.userIdToUserSocket.delete(user.id);
+    this.socketIdToUserSocket.delete(socketId);
+  }
+
+  all() {
+    return Array.from(this.socketIdToUserSocket.values());
+  }
+}
+
 const rooms = new Map<string, Room>();
-const sockets = new Map<string, UserSocket>();
+const socketsCollection = new UserSocketCollection();
 const streams = new Map<string, VideoStream>();
 const usersCache = new Map<string, User>();
 
@@ -34,10 +59,11 @@ io.on('connection', (socket) => {
   log(`Socket ${socket.id} connected`);
   streams.set(socket.id, new VideoStream(socket));
 
-  socket.on('set-user-info', (params: { userId: string }) => {
+  socket.on('set-user-info', (params: { userId: string }, callback?: () => void) => {
     log(`Setting user id ${params.userId} to socket ${socket.id}`);
     const userSocket = new UserSocket(usersCache.get(params.userId)!, socket);
-    sockets.set(socket.id, userSocket);
+    socketsCollection.add(userSocket);
+    callback?.();
   });
 
   socket.on('stream-video', (data) => {
@@ -51,19 +77,21 @@ io.on('connection', (socket) => {
   });
 
   socket.on('join-room', async ({ roomId }: { roomId: string }, callback?: () => void) => {
-    log(`Joining in the room ${roomId}`);
+    const userSocket = socketsCollection.getBySocketId(socket.id);
+    const user = userSocket!.user;
+    log(`Joining ${user.id} in the room ${roomId}`);
 
+    rooms.get(roomId)?.addUser(user);
     await socket.join(roomId);
-    const userSocket = sockets.get(socket.id);
-    rooms.get(roomId)?.addUser(userSocket!.user);
-    io.to(roomId).emit('joined-user', userSocket?.user);
+    console.log(io.sockets.adapter.rooms);
+    io.to(roomId).emit('joined-user', { user });
     callback?.();
   });
 
   socket.on('disconnect', () => {
     log(`Socket ${socket.id} disconnected`);
     streams.get(socket.id)?.destroy();
-    sockets.delete(socket.id);
+    socketsCollection.delete(socket.id);
   });
 });
 
@@ -84,13 +112,14 @@ app.post('/users', (request, response) => {
   return response.json(user.render());
 });
 
-app.post('/rooms', (request, response) => {
+app.post('/rooms', async (request, response) => {
   const userId = String(request.header('userId'));
   const user = usersCache.get(userId)!;
   log(`Creating room to user ${user.id}`);
   const room = new Room();
   room.addUser(user);
   rooms.set(room.id, room);
+  await socketsCollection.getByUserId(userId)?.socket.join(room.id);
   return response.json(room.render());
 });
 
@@ -102,7 +131,7 @@ app.get('/rooms/:roomId', (request, response) => {
 });
 
 function handleAppError() {
-  Array.from(sockets.values()).forEach((socket) => socket.socket.disconnect(true));
+  socketsCollection.all().forEach((socket) => socket.socket.disconnect(true));
   Array.from(streams.values()).forEach((stream) => stream.destroy());
   io.close(() => console.log('Socket server closed'));
   server.close(() => console.log('HTTP Server closed'));
